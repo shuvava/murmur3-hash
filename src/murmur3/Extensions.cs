26c1805f6bbf9a07cdddfd06438d7b403ca17dd0
geek <keeper2002@mail.ru>
2018-09-27 21:14:16 -0700

    code refactoring

--- src/murmur3/Extensions.cs
+++
@@ -1,6 +0,0 @@
-namespace murmur
-{
-    internal static class Extensions
-    {
-    }
-}
--- src/murmur3/MurmurBase.cs
+++ src/murmur3/MurmurBase.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Security.Cryptography;
 
 
@@ -6,17 +5,18 @@ namespace murmur
 {
     public abstract class MurmurBase : HashAlgorithm
     {
-
-        public MurmurBase(uint seed = 0)
+        protected MurmurBase(uint seed = 0)
         {
             Seed = seed;
         }
+
 
         protected uint Seed { get; }
 
         protected int Length { get; set; }
 
         protected abstract void Reset();
+
 
         public override void Initialize()
         {
